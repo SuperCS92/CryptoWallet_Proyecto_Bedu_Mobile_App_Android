a36814406106572c8736bcb1e1ee1391d6bcdb87
PaulinaQuintero <paulina.mucito@gmail.com>
2021-10-17 16:38:53 -0500

    Animations Added

--- app/src/main/java/com/example/wallet2/RecyclerAdapter_AssetActivity.kt
+++ app/src/main/java/com/example/wallet2/RecyclerAdapter_AssetActivity.kt
@@ -2,18 +2,13 @@ package com.example.wallet2
 
 import android.app.Dialog
 import android.content.Context
-import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentActivity
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
-import java.util.logging.Handler
 
 class RecyclerAdapter_AssetActivity(
     var context: Context?,
@@ -82,7 +77,13 @@ class RecyclerAdapter_AssetActivity(
                 val transaction = fragmentManager.beginTransaction()
                 transaction.replace(R.id.fragment_container, transactionFragment)
                 transaction.commit()*/
-                Navigation.findNavController(it).navigate(R.id.action_dashboardFragmentDest_to_transactionFragment, null)
+                if(Navigation.findNavController(it).currentDestination?.id == R.id.dashboardFragmentDest) {
+                    Navigation.findNavController(it)
+                        .navigate(R.id.action_dashboardFragmentDest_to_transactionFragment, null)
+                } else if(Navigation.findNavController(it).currentDestination?.id == R.id.assetFragment){
+                    Navigation.findNavController(it)
+                        .navigate(R.id.action_assetFragment_to_transactionFragment, null)
+                }
 
             }
         }
